2d54150aba13c384525754edea1cacceded76fc5
vntero <shin.antero@gmail.com>
2022-03-24 17:40:11 +0100

    gitignore file had a typo

--- receive.js
+++ receive.js
@@ -17,7 +17,9 @@ amqp.connect('amqp://localhost', function(error0, connection) {
     channel.assertQueue(queue, {
       durable: false
     });
+
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    
     channel.consume(queue, function(msg) {
       console.log(" [x] Received %s", msg.content.toString());
     }, {
